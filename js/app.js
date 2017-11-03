3a39c77ab634e52c68b7b9f0452f1babe68ac0d5
Serene Liu <cloud9@sentanother.email>
2017-11-03 00:18:34 +0000

    appended li classes and shuffled cards

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 /*
  * Create a list that holds all of your cards
  */
-
+var cards = ["fa fa-diamond", "fa fa-diamond", "fa fa-paper-plane-o", "fa fa-paper-plane-o", "fa fa-anchor", "fa fa-anchor", "a fa-bolt", "a fa-bolt", "fa fa-cube", "fa fa-cube", "fa fa-leaf", "fa fa-leaf", "fa fa-bicycle", "fa fa-bicycle", "fa fa-bomb", "fa fa-bomb"];
 
 /*
  * Display the cards on the page
@@ -9,6 +9,16 @@
  *   - loop through each card and create its HTML
  *   - add each card's HTML to the page
  */
+for (x = 0; x < 16; x++) {
+    $(".deck").append("<li class='card'><i class='name'></i></li>");
+}
+
+shuffle(cards);
+console.log(cards);
+
+for (y = 0; y < cards.length; y++) {
+    $(".deck").replace("name", cards[y]);
+}
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
